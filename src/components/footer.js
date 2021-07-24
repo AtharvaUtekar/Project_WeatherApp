d7a05336f203a7906a11173f3f012517b0053f18
Atharva Utekar <65060103+AtharvaUtekar@users.noreply.github.com>
2021-07-24 13:12:21 +0530

    Add files via upload

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.css";
+
+
+const footer = () => {
+    return (
+        <div className="Footer">
+        By Atharva Utekar        
+      </div>
+    )
+}
+
+export default footer;
